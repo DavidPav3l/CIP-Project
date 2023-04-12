921da4b7799aad4613c6b21253366eff09d44ce8
DavidPav3l <david_marian@outlook.com>
2023-04-12 13:11:27 +0300

    Am aranjat pe foldere separate pt. ca e mai usor asa.

---
+++ front-end/tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    content: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'media',
+    theme: {
+        screens: {
+            xxs: '350px',
+            xs: '475px',
+            sm: '640px',
+            md: '768px',
+            lg: '1024px',
+            xl: '1280px',
+            '2xl': '1536px',
+            '3xl': '1700px',
+        },
+        extend: {
+            colors: {},
+            fontFamily: {},
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+};
