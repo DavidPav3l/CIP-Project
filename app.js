b57b7923de3def85967e155e55cc88f639abca80
DavidPav3l <david_marian@outlook.com>
2023-04-14 02:30:41 +0300

    avem script de build pt vercel

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const app = express();
-const port = 6060;
+const port = process.env.PORT || 6060;
 
 app.use(express.static('./front-end/public'));
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}...`);
+  console.log(`Listening on port ${port}...`);
 });
