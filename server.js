6f40697edf759109f92c1b0d89751e22938aa499
DavidPav3l <david_marian@outlook.com>
2023-04-14 23:03:10 +0300

    wow

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 6060;
 
-app.use(express.static('./public'));
+app.use(express.static('./front-end/public'));
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
