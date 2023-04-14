37a26f8f1a86bd421288ce126b33016a83c3cd28
DavidPav3l <david_marian@outlook.com>
2023-04-15 00:28:12 +0300

    doamnee ajuta

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 6060;
 
-app.use(express.static('./front-end/public'));
+app.use(express.static(__dirname + '/front-end/public'));
 
 app.listen(port, '0.0.0.0', () => {
   console.log(`Listening on port ${port}...`);
