df1c1b87969182597eb557ebffc288a427095b64
DavidPav3l <david_marian@outlook.com>
2023-04-14 23:11:36 +0300

    acu e acu

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const port = process.env.PORT || 6060;
 
 app.use(express.static('./front-end/public'));
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`Listening on port ${port}...`);
 });
